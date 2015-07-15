59fa20a5201bd61e40450628ecfcd462ce07a1c9
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2015-07-15 15:10:06 -0500

    Use cleared field to calculate metrics

--- AiBlockBattle.scala
+++ AiBlockBattle.scala
@@ -119,8 +119,37 @@ class Field(string: String) {
     cell == 0 || cell == 1
   }
 
+  def isClearableBlock(block: Block): Boolean = {
+    val (row, col) = block
+
+    if (row < 0 || row >= height || col < 0 || col >= width)
+      return false
+
+    array(row)(col) == 2
+  }
+
   def moveValid(move: Set[Block]): Boolean = {
     move forall isEmpty
+  }
+}
+
+class MovedField(field: Field, blocks: Set[(Int, Int)]) {
+  type Block = (Int, Int)
+  private val beforeRowClear = for (row <- 0 until field.height; col <- 0 until field.width; if field.isClearableBlock((row, col))) yield (row, col)
+  private val combined = (beforeRowClear ++ blocks).toSet
+  private val clearedRows = (combined groupBy {_._1} filter {_._2.size == field.width} map {_._1}).toSet
+  private def removeRow(accum: (Int, Set[Block]), row: Int): (Int, Set[Block]) = {
+    val (offset, result) = accum
+    if (clearedRows contains row)
+      (offset + 1, result)
+    else
+      (offset, result ++ (combined filter {_._1 == row} map {case (x, y) => (x + offset, y)}))
+  }
+
+  val (clearCount, cleared) = (field.height - 1 to 0 by -1).foldLeft((0, Set[Block]()))(removeRow)
+
+  def isEmpty(block: Block): Boolean = {
+    !(cleared contains block)
   }
 }
 
@@ -128,11 +157,13 @@ case class Metric(blocks: Set[(Int, Int)], positions: Set[((Int, Int), Int)], fi
   type Block = (Int, Int)
   type Position = (Block, Int) // Origin, angle
 
+  private lazy val movedField = new MovedField(field, blocks)
+
   lazy val blockHeight = blocks.toList.map(_._1).sum
 
   lazy val holeCount = {
     val colHoles = for (col <- 0 until field.width) yield {
-      def empty(row: Int) = field.isEmpty((row, col)) && !blocks.contains((row, col))
+      def empty(row: Int) = movedField.isEmpty((row, col))
       (0 until field.height) dropWhile empty count empty
     }
     colHoles.sum
