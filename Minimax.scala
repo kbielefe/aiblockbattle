ade20622dc7d3534cbe8878adbe80a33984fbc68
Karl Bielefeldt <karl.bielefeldt@adtran.com>
2015-07-17 14:00:27 -0500

    Partial implementation of Score

--- Minimax.scala
+++ Minimax.scala
@@ -1,9 +1,17 @@
-class Minimax[Node](lt: (Node, Node) => Boolean) {
+class Minimax[Node](lt: (Node, Node) => Boolean, terminal: Node => Boolean, heuristic: Node => Score) {
   sealed abstract class Score {
     def <(other: Score): Boolean
 
     def <=(other: Score): Boolean = {
       this < other || this == other
+    }
+
+    def >(other: Score): Boolean = {
+      !(this <= other)
+    }
+
+    def >=(other: Score): Boolean = {
+      !(this < other)
     }
 
     def min(other: Score): Score = {
@@ -18,6 +26,17 @@ class Minimax[Node](lt: (Node, Node) => Boolean) {
         other
       else
         this
+    }
+
+    def   <(other: Node): Boolean = this  < FiniteScore(other)
+    def  <=(other: Node): Boolean = this <= FiniteScore(other)
+    def   >(other: Node): Boolean = this  > FiniteScore(other)
+    def  >=(other: Node): Boolean = this >= FiniteScore(other)
+    def min(other: Node): Score = min(FiniteScore(other))
+    def max(other: Node): Score = max(FiniteScore(other))
+
+    def getNode: Node = {
+      throw new Exception("Doesn't have a node")
     }
   }
 
@@ -38,9 +57,13 @@ class Minimax[Node](lt: (Node, Node) => Boolean) {
       case Infinity         => true
       case FiniteScore(otherNode) => lt(node, otherNode)
     }
+
+    override def getNode: Node = node
   }
 
   def alphabeta(node: Node, depth: Int, alpha: Score, beta: Score, maximizing: Boolean): Score = {
+    if (depth == 0 || terminal(node))
+      return heuristic(node)
     Infinity
   }
 }
